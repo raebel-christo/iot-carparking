57f902f0c5c50b7b7b3d005d42c356f385eb12d6
raebel-christo <raebel.christo@gmail.com>
2023-05-01 00:52:57 +0530

    Updated Files

---
+++ python/imageclient.py
@@ -0,0 +1,36 @@
+import picamera
+import struct
+import time
+import socket
+import io
+
+websocket = socket.socket()
+websocket.bind('192.168.1.101', 8000)
+
+connection = websocket.makefile('wb')
+
+try:
+
+    camera = picamera.PiCamera()
+    camera.resolution = (500,480)
+    camera.start_preview()
+    time.sleep(2)
+
+    start = time.time()
+    stream = io.bytesIO()
+
+    for foo in camera.capture_continuous(stream, 'jpeg'):
+        connection.write(struct.pack('<L', stream.tell()))
+        connection.flush()
+        stream.seek(0)
+        connection.write(stream.read())
+        stream.seek(0)
+        stream.truncate()
+
+except KeyboardInterrupt:
+    print("Terminating Camera Stream")
+    connection.write(struct.pack('<L', 0))
+
+finally:
+    connection.close()
+    websocket.close()
