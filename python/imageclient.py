659189b1bcf66cb38f03c254e58c30a104f8ded9
raebel-christo <raebel.christo@gmail.com>
2023-05-01 21:23:43 +0530

    Modified camera ouput

--- python/imageclient.py
+++ python/imageclient.py
@@ -1,8 +1,8 @@
 import picamera
 import struct
-import time
 import socket
 import io
+import time
 
 websocket = socket.socket()
 websocket.connect(('192.168.1.101', 8000))
@@ -13,10 +13,11 @@ try:
 
     camera = picamera.PiCamera()
     camera.resolution = (500,480)
+    camera.vflip = True
+    camera.hflip = True
     camera.start_preview()
     time.sleep(2)
 
-    start = time.time()
     stream = io.BytesIO()
 
     for foo in camera.capture_continuous(stream, 'jpeg'):
@@ -26,6 +27,7 @@ try:
         connection.write(stream.read())
         stream.seek(0)
         stream.truncate()
+        time.sleep(0.5)
 
 except KeyboardInterrupt:
     print("Terminating Camera Stream")
